fe00a14a1292d789baa0f429d3f8ce8443b3189b
shweta2411 <63298504+shweta2411@users.noreply.github.com>
2020-06-02 15:53:01 +0530

    Add files via upload

---
+++ scalerfile.py
@@ -0,0 +1,144 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+df = pd.read_csv('readmission.csv')
+
+
+# replace ? with nan
+df = df.replace('?',np.nan)
+
+cols_num = ['time_in_hospital','num_lab_procedures', 'num_procedures', 'num_medications',
+       'number_outpatient', 'number_emergency', 'number_inpatient','number_diagnoses']
+df[cols_num].isnull().sum()
+cols_cat = ['race', 'gender', 
+       'max_glu_serum', 'A1Cresult',
+       'metformin', 'repaglinide', 'nateglinide', 'chlorpropamide',
+       'glimepiride', 'acetohexamide', 'glipizide', 'glyburide', 'tolbutamide',
+       'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone',
+       'tolazamide', 'insulin',
+       'glyburide-metformin', 'glipizide-metformin',
+       'glimepiride-pioglitazone', 'metformin-rosiglitazone',
+       'metformin-pioglitazone', 'change', 'diabetesMed','payer_code']
+df[cols_cat].isnull().sum()
+df['race'] = df['race'].fillna('UNK')
+df['payer_code'] = df['payer_code'].fillna('UNK')
+df['medical_specialty'] = df['medical_specialty'].fillna('UNK')
+
+df.groupby('medical_specialty').size().sort_values(ascending = False)
+top_10 = ['UNK','InternalMedicine','Emergency/Trauma',\
+          'Family/GeneralPractice', 'Cardiology','Surgery-General' ,\
+          'Nephrology','Orthopedics',\
+          'Orthopedics-Reconstructive','Radiologist']
+
+# make a new column with duplicated data
+df['med_spec'] = df['medical_specialty'].copy()
+
+# replace all specialties not in top 10 with 'Other' category
+df.loc[~df.med_spec.isin(top_10),'med_spec'] = 'Other'
+
+df.groupby('med_spec').size()
+cols_cat_num = ['admission_type_id', 'discharge_disposition_id', 'admission_source_id']
+
+df[cols_cat_num] = df[cols_cat_num].astype('str')
+df_cat = pd.get_dummies(df[cols_cat + cols_cat_num + ['med_spec']],drop_first = True)
+
+df = pd.concat([df,df_cat], axis = 1)
+
+cols_all_cat = list(df_cat.columns)
+
+df[['age', 'weight']].head()
+df.groupby('age').size()
+age_id = {'[0-10)':0, 
+          '[10-20)':10, 
+          '[20-30)':20, 
+          '[30-40)':30, 
+          '[40-50)':40, 
+          '[50-60)':50,
+          '[60-70)':60, 
+          '[70-80)':70, 
+          '[80-90)':80, 
+          '[90-100)':90}
+df['age_group'] = df.age.replace(age_id)
+
+df.weight.notnull().sum()
+df['has_weight'] = df.weight.notnull().astype('int')
+cols_extra = ['age_group','has_weight']
+df[cols_num + cols_all_cat + cols_extra].isnull().sum().sort_values(ascending = False).head(10)
+col2use = cols_num + cols_all_cat + cols_extra
+df_data = df[col2use + ['OUTPUT_LABEL']]
+# shuffle the samples
+df_data = df_data.sample(n = len(df_data), random_state = 42)
+df_data = df_data.reset_index(drop = True)
+# Save 30% of the data as validation and test data 
+df_valid_test=df_data.sample(frac=0.30,random_state=42)
+df_test = df_valid_test.sample(frac = 0.5, random_state = 42)
+df_valid = df_valid_test.drop(df_test.index)
+
+# use the rest of the data as training data
+df_train_all=df_data.drop(df_valid_test.index)
+# split the training data into positive and negative
+rows_pos = df_train_all.OUTPUT_LABEL == 1
+df_train_pos = df_train_all.loc[rows_pos]
+df_train_neg = df_train_all.loc[~rows_pos]
+
+# merge the balanced data
+df_train = pd.concat([df_train_pos, df_train_neg.sample(n = len(df_train_pos), random_state = 42)],axis = 0)
+
+# shuffle the order of training samples 
+df_train = df_train.sample(n = len(df_train), random_state = 42).reset_index(drop = True)
+df_train_all.to_csv('df_train_all.csv',index=False)
+df_train.to_csv('df_train.csv',index=False)
+df_valid.to_csv('df_valid.csv',index=False)
+df_test.to_csv('df_test.csv',index=False)
+X_train = df_train[col2use].values
+X_train_all = df_train_all[col2use].values
+X_valid = df_valid[col2use].values
+
+y_train = df_train['OUTPUT_LABEL'].values
+y_valid = df_valid['OUTPUT_LABEL'].values
+from sklearn.preprocessing import StandardScaler
+
+scaler  = StandardScaler()
+scaler.fit(X_train_all)
+
+import pickle
+scalerfile = 'scaler.sav'
+pickle.dump(scaler, open(scalerfile, 'wb'))
+
+# load it back
+scaler = pickle.load(open(scalerfile, 'rb'))
+X_train_tf = scaler.transform(X_train)
+X_valid_tf = scaler.transform(X_valid)
+
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
+def calc_specificity(y_actual, y_pred, thresh):
+    # calculates specificity
+    return sum((y_pred < thresh) & (y_actual == 0)) /sum(y_actual ==0)
+
+def print_report(y_actual, y_pred, thresh):
+    
+    auc = roc_auc_score(y_actual, y_pred)
+    accuracy = accuracy_score(y_actual, (y_pred > thresh))
+    recall = recall_score(y_actual, (y_pred > thresh))
+    precision = precision_score(y_actual, (y_pred > thresh))
+    specificity = calc_specificity(y_actual, y_pred, thresh)
+thresh = 0.5
+   
+
+# k-nearest neighbors
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors = 100)
+knn.fit(X_train_tf, y_train)
+y_train_preds = knn.predict_proba(X_train_tf)[:,1]
+y_valid_preds = knn.predict_proba(X_valid_tf)[:,1]
+
+print('KNN')
+print('Training:')
+knn_train_auc, knn_train_accuracy, knn_train_recall, \
+    knn_train_precision, knn_train_specificity = print_report(y_train,y_train_preds, thresh)
+print('Validation:')
+knn_valid_auc, knn_valid_accuracy, knn_valid_recall, \
+    knn_valid_precision, knn_valid_specificity = print_report(y_valid,y_valid_preds, thresh)
